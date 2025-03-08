3db14a524f61155cfc444901e343c89e0d559441
xiaoluabei <xiaoluabei@gmail.com>
2025-03-08 09:55:03 +0800

    chore: reorganize folder structure

---
+++ backend/express.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  { id: "1", content: "HTML is easy", important: true },
+  { id: "2", content: "Browser can execute only JavaScript", important: false },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((n) => n.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((n) => n.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const newNote = {
+    id: String(notes.length + 1),
+    body: body.content,
+    important: body.important || false,
+  };
+  notes.push(newNote);
+  response.json(newNote);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
